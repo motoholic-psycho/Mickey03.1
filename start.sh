ffffd9d409c76637e5d7324263a3e94d6bb7e705
Muhammedshamil03 <105405467+Muhammedshamil03@users.noreply.github.com>
2022-06-19 21:24:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/PROFESSOR-BOT.git /PROFESSOR-BOT
+  git clone https://github.com/Muhammedshamil03/Mickey03.git /PROFESSOR-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PROFESSOR-BOT
 fi
 cd /PROFESSOR-BOT
 pip3 install -U -r requirements.txt
-echo "Starting 𝙼𝙺𝙽 𝙱𝙾𝚃𝚉....🔥🔥"
+echo "Starting 𝙼ICKEY 𝙱𝙾𝚃....🔥🔥"
 python3 bot.py
